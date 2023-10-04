f7e20b01469e57bd58c19dd6bc645ed3f661eb2d
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-10-04 10:48:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const lowerCaseWord = word.toLowerCase()
+  const reversedWord = lowerCaseWord.split('').reverse().join('');
+  return lowerCaseWord === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  the function will receive one argument, a string.
+   the function will then return true if the string is a palindrome 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+  
+    console.log("");
+  
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+  
+    console.log("");
+  
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
 
 module.exports = isPalindrome;
